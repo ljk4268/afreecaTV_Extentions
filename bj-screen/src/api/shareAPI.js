5ebbc6d1b8d311d5748d2b1bc8264d7fa2e6f911
ljk4268 <ljk4268@kakao.com>
2023-12-01 16:32:34 +0900

    bj화면

--- bj-screen/src/api/shareAPI.js
+++ bj-screen/src/api/shareAPI.js
@@ -1,21 +1,24 @@
 import { api } from './utils/instance'
 
 //axios
+
+// 공유된 정보 가져오는 메소드
 export function getItems(params) {
   return api.post('/read', params)
 }
+// 이미지 가져오는 메소드
 export function getImage(params) {
   return api.post('/image', params, { responseType: 'blob' })
 }
-
+// 정보 추가하는 메소드
 export function addItems(params) {
   return api.post('/create', params, { 'Content-Type': 'multipart/form-data' })
 }
-
+// 정보 수정하는 메소드
 export function editItems(params) {
   return api.post('/update', params, { 'Content-Type': 'multipart/form-data' })
 }
-
+// 정보 삭제하는 메소드
 export function deleteItems(params) {
   return api.post('/delete', params)
 }
